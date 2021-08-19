a9a951042a2c8b634e176fd9e9b841a2f3727891
cbermudez <cesar.bermudez@uruit.com>
2021-08-19 07:31:12 -0500

    11- Displaying Validation Errors

---
+++ src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const input = ({name, label, value, error, onChange}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        value={value}
+        onChange={onChange}
+        name={name}
+        id={name}
+        type="text"
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default input;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,18 +1,60 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 class LoginForm extends Component {
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "") {
+      errors.username = "Username is required";
+    }
+    if (account.password.trim() === "") {
+      errors.password = "Password is required";
+    }
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+    // Call the server
+    console.log("Submitted", this.state.account);
+  };
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form action="">
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+            error={errors.username}
+          ></Input>
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+            error={errors.password}
+          ></Input>
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
